669e47eb9c6e7c1a2a441de878e2b031cc1746f5
Jose Jorge <josejorge@JJs-MacBook-Pro.local>
2019-06-16 11:42:33 +0100

    Project creation

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,13 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
+  apiKey: 'AIzaSyBdCR4UOCoFyQunGiFdaOV5Yk7xY0XbFpY',
+  authDomain: 'scp-performance.firebaseapp.com',
+  databaseURL: 'https://scp-performance.firebaseio.com',
+  projectId: 'scp-performance',
+  storageBucket: 'scp-performance.appspot.com',
+  messagingSenderId: '285332299451',
+  appId: '1:285332299451:web:102b2c9c832caf63'
 }
 
 firebase.initializeApp(config)
