f92e21d5d4785e22ecca30aa3fae9c953399c849
Jose Jorge <josejorge@JJs-MacBook-Pro.local>
2019-06-16 11:31:04 +0100

    :tada: Initial commit

---
+++ src/store/app/app.state.js
@@ -0,0 +1,7 @@
+export default {
+  appTitle: process.env.VUE_APP_TITLE,
+  networkOnLine: true,
+  SWRegistrationForNewContent: null,
+  showAddToHomeScreenModalForApple: false,
+  refreshingApp: false
+}
